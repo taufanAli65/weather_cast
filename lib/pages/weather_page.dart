35ea7f67c474a3d487e7933cc2ced9449bd2f12c
Taufan Ali <126685412+taufanAli65@users.noreply.github.com>
2024-07-20 06:44:38 +0700

    some update on the interface

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:intl/intl.dart';
 import 'package:weather_cast/models/weather_model.dart';
 import 'package:weather_cast/services/weather_service.dart';
 
@@ -33,26 +34,22 @@ class _WeatherPageState extends State<WeatherPage> {
   String getWeatherAnimation(String? mainCondition) {
     if (mainCondition == null) return 'assets/cerah.json'; //default auto sunny
 
-    switch(mainCondition.toLowerCase()) {
+    switch (mainCondition.toLowerCase()) {
       case 'clouds':
-      case 'mist':
-      case 'smoke':
-      case 'haze':
-      case 'dust':
+        return 'assets/cerah-berawan.json';
       case 'fog':
         return 'assets/cerah-berawan.json';
       case 'rain':
-      case 'drizzle':
+        return 'assets/hujan-ringan.json';
       case 'shower rain':
         return 'assets/hujan-ringan.json';
       case 'thunderstorm':
         return 'assets/hujan-deras.json';
       case 'clear':
         return 'assets/cerah.json';
-      default :
+      default:
         return 'assets/cerah.json';
-
-    } 
+    }
   }
 
   @override
@@ -63,15 +60,74 @@ class _WeatherPageState extends State<WeatherPage> {
 
   @override
   Widget build(BuildContext context) {
+    final DateTime now = DateTime.now();
+    final String formattedDate = DateFormat('EEEE, MMM d, yyyy').format(now);
+    final String formattedTime = DateFormat('hh:mm a').format(now);
+
     return Scaffold(
+      backgroundColor: Color(0xFF444575),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(_weather?.cityName ?? "Memuat Kota..."),
+            Padding(
+              padding: const EdgeInsets.only(top: 40.0), // Padding at the top
+              child: Column(
+                children: [
+                  Text(
+                    formattedDate,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    formattedTime,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    _weather?.cityName ?? "Memuat Kota...",
+                    style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Spacer(), // Spacer to push the Lottie animation to the center
             Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
-            Text(_weather != null ? '${_weather!.temperature.round()} C' : 'Memuat Temperatur...'),
-            Text(_weather?.mainCondition ?? "Memuat Cuaca...")
+            Spacer(), // Spacer to push the temperature and main condition to the bottom
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15.0),
+              child: Text(
+                _weather != null ? '${_weather!.temperature.round()}°C' : 'Memuat Temperatur...',
+                style: TextStyle(
+                  fontSize: 60.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50.0),
+              child: Text(
+                _weather?.mainCondition ?? "Memuat Cuaca...",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ],
         ),
       ),
